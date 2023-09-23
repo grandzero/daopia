cf71d704e8c9d1922f2bd3ab9d695b654d71f18a
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-09-23 17:30:15 +0300

    working on testnet

--- scripts/op.ts
+++ scripts/op.ts
@@ -2,35 +2,37 @@ import { ethers } from "hardhat";
 import { Database } from "@tableland/sdk";
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || "", ethers.provider)
 async function main() {
-  const daopia = await ethers.getContractAt("Daopia", "0x23a19F0DD0a65ddDf9aBd7A5C0A105D018f69e39");
-  let tableId = await daopia.proposalsTableId();
-  console.log("Table ID is : ", tableId.toString());
+  const daopia = await ethers.getContractAt("Daopia", "0x9f0107446A47b2D56e4572301cE29679b02dd146");
+  //let tableId = await daopia.proposalsTableId();
+ // console.log("Table ID is : ", tableId.toString());
+  // const accounts = await ethers.getSigners();
+//   const registrationDetails = {
+//     period: 3600,
+//     price: ethers.utils.parseEther("0.1"),
+//     isBalanceLocked: false,
+//     paymentType: 1, // PaymentType.Ether
+//     paymentContract: ethers.constants.AddressZero, // This is for Ether payment
+//     vault: wallet.address, // Owner should be the vault for this test
+//     registrationStatus: 0, // RegistrationStatus.Open
+//   };
 
-  // const registrationDetails = {
-  //   period: 3600,
-  //   price: ethers.utils.parseEther("0.1"),
-  //   isBalanceLocked: false,
-  //   paymentType: 1, // PaymentType.Ether
-  //   paymentContract: ethers.constants.AddressZero, // This is for Ether payment
-  //   vault: wallet.address, // Owner should be the vault for this test
-  //   registrationStatus: 0, // RegistrationStatus.Open
-  // };
+//   const dealDetails = {
+//     num_copies:1,
+//     repair_treshold: 28800,
+//     renew_treshold: 28800,
+//   }
+// // Ensure the owner (accounts[0]) is calling the function
+//  await daopia.registerDao(registrationDetails, dealDetails);
+// const daoDetails = await daopia.daoDetails(wallet.address);
+//     console.log(daoDetails.vault, wallet.address);
+  //await daopia.makeProposalToDao(wallet.address, "Proposal 17");
 
-  // const dealDetails = {
-  //   num_copies:1,
-  //   repair_treshold: 28800,
-  //   renew_treshold: 28800,
-  // }
-// Ensure the owner (accounts[0]) is calling the function
-// await daopia.registerDao(registrationDetails, dealDetails);
-//const daoDetails = await daopia.daoDetails(wallet.address);
-    //console.log(daoDetails.vault, wallet.address);
-  //await daopia.makeProposalToDao(wallet.address, "Proposal 1");
+  //let num = Number(tableId);
+  // const tableName = "daopia"+"_31337_" + num; 
+  //await daopia.approveProposal("Proposal1",1);
+  const tableName = "daopia_314159_369" //+num; // Our pre-defined health check table
 
-
-  const tableName = "daopia_proposals_314159_368"; // Our pre-defined health check table
-
-  const db = new Database({autoWait: false});
+  const db = new Database({autoWait: false, signer: wallet});
 
   const { results } = await db.prepare(`SELECT * FROM ${tableName};`).all();
   console.log(results);
